29156907423a13adba6e05402ad1f2791fcf58ad
dima <az0908.play@gmail.com>
2025-11-12 21:42:06 +0200

    Add files via upload

---
+++ dz c++ 7237723/Project1/Project1/FileName.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+   /* int a;
+    cin >> a; 
+
+    for (int i = 0; i < a; i++) {
+        for (int j = 0; j < a; j++) {
+            if (i >= j) cout << "*";
+            else cout << " ";
+        }
+        cout << endl;
+    }*/
+
+
+    /*int a;
+    cin >> a; 
+
+    for (int i = 0; i < a; i++) {
+        for (int j = 0; j < a; j++) {
+            if (i <= j) cout << "*";
+            else cout << " ";
+        }
+        cout << endl;
+    }*/
+
+
+    int a;
+    cin >> a; 
+
+    for (int i = 0; i < a; i++) {
+        for (int j = 0; j < a; j++) {
+            if (i + j >= a - 1) cout << "*";
+            else cout << " ";
+        }
+        cout << endl;
+    }
+}
