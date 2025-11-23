8af1e078af5b12ea9dc83c24adac8fef31ffd8b3
dima <az0908.play@gmail.com>
2025-11-23 17:25:21 +0200

    Add files via upload

---
+++ дзс++/Project1/Project1/FileName.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    int a;
+    int b = 0;      
+    int c = 0;
+
+    while (true) {
+        cin >> a;
+
+        if (a == -10) {
+            break; 
+        }
+
+        if (a % 2 == 0) {
+            b += a; 
+        }
+        else {
+            c++;
+        }
+    }
+
+    cout << b << endl;
+    cout << c << endl;
+
+    return 0;
+}
